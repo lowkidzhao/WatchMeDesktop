9c4e7fdc04227e118b2f657ebc66b6aec1ad36ef
pushme <1141846557@qq.com>
2024-06-22 15:45:43 +0800

    集成react(next)

--- preload.js
+++ preload.js
@@ -1,8 +1,7 @@
-const { contextBridge } = require('electron')
+const { contextBridge } = require('electron/renderer')
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron
-    // 除函数之外，我们也可以暴露变量
 })
